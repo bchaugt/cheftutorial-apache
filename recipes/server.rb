1cb6925679e9defa0ec953fc949202be8d332314
vagrant <vagrant@localhost.localdomain>
2017-02-17 03:18:02 +0000

    updated default recipe to use template for index.html

--- recipes/server.rb
+++ recipes/server.rb
@@ -8,14 +8,8 @@ package 'httpd' do
   action :install
 end
 
-file '/var/www/html/index.html' do
-  content "<h1>Hello, world!</h1>
-  <h3>This is Brian's Apache server</h3>
-  HOSTNAME: #{node['hostname']}<br>
-  IPADDRESS: #{node['ipaddress']}<br>
-  CPU: #{node['cpu']['0']['mhz']}<br>
-  MEMORY: #{node['memory']['total']}<br>
-"
+template '/var/www/html/index.html' do
+  source 'index.html.erb'
 end
 
 service 'httpd' do
